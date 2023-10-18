d796dd98a45be2e75f379d47dd7d8b5790547ab1
mohraf <amosngok@gmail.com>
2023-10-18 12:53:28 +0300

    feat: edit theme

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,8 +2,8 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
+    --dark-navy: #152724;
+    --navy: #18332f;
     --light-navy: #112240;
     --lightest-navy: #233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
@@ -12,10 +12,11 @@ const variables = css`
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #64ffda;
+    --green: #ffc300; //orange
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
+    --orange: #ffa500; // originalgreen#64ffda
 
     --font-sans: 'Calibre', 'Inter', 'San Francisco', 'SF Pro Text', -apple-system, system-ui,
       sans-serif;
